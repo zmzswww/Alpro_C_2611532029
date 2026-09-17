6fe35831c1e88149b78e49e9f4ca8a3dcda23d5e
username <emailnya>
2026-09-17 12:02:17 +0700

    Update Prak 3

--- pekan2/Tugas2_2611532029.py
+++ pekan2/Tugas2_2611532029.py
@@ -14,9 +14,7 @@ nama_2029 = input("Masukkan Nama Mahasiswa     : ")
 jk_2029 = input("Masukkan Jenis Kelamin (L/P): ")
 
 # Menyimpan alamat tempat tinggal secara multiline menggunakan tanda petik tiga
-alamat_2029 = """Jl. Kampus Unand,
-Kecamatan Pauh,
-Kota Padang"""
+alamat_2029 = """Jl. Kampus Unand, Kecamatan Pauh, Kota Padang"""
 
 
 # === INPUT DATA & TYPE CASTING (Tipe Data Numerik) ===
